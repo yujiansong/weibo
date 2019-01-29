655f9a40d8373a8fc48f661679d74edcd5ca9214
yjs <yujiansong6@163.com>
2019-01-29 03:19:24 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
+            <a href="https://github.com/yujiansong">
+                业精于勤，荒于嬉；行成于思，毁于随
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.default')
-@section('title')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>hello, friend</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="https://github.com/yujiansong">cuckoo微博</a> 项目的主页。
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+        </p>
+    </div>
 @stop

ed270b24dd5b66416e0d0d130ea5efd887652b0b
yjs <yujiansong6@163.com>
2019-02-01 03:34:21 +0000

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = factory(\App\Models\User::class)->times(5)->make();
+        $users = factory(\App\Models\User::class)->times(50)->make();
         \App\Models\User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = \App\Models\User::find(1);
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
             'email' => 'ok520koa@163.com',
             'password' => bcrypt('123456'),
             'is_admin' => true,
+            'activated' => true,
         ]);
     }
 }
